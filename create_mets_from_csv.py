32106eb16f46d160fad635102a30444bda5cdbd8
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2021-10-07 09:28:16 +0200

    adapted the script for creating mets

--- create_mets_from_csv.py
+++ create_mets_from_csv.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 import jinja2
 import os
+import shutil
 
 map_authors = {
     "EMT": {
@@ -29,7 +30,7 @@ map_authors = {
 @plac.opt("output_dir", "folder for the output METS files", type=Path)
 @plac.opt("mets_template", "Path to METS template relative to current directory", type=Path)
 @plac.opt("rows", "Rows to process from CSV (eg 20-30) -> str")
-def main(folder, metadata, output="output/", mets_template="mets_template.xml", rows=False):
+def main(folder, metadata, output="output/", mets_template="mets_template.xml", rows=None):
     metadata_df = pd.read_csv(metadata)
     templateLoader = jinja2.FileSystemLoader(searchpath=".")
     templateEnv = jinja2.Environment(loader=templateLoader)
@@ -42,17 +43,30 @@ def main(folder, metadata, output="output/", mets_template="mets_template.xml", 
         end_row = metadata_df.shape[0]
     folder_name = None
     doc = []
-    for index, row in metadata_df[start_row:end_row].iterrows():
+    count_0 = 0
+    for index, row in metadata_df.iterrows():
         print(row)
         print(folder_name)
         print(row["folder"])
         if folder_name != row["folder"]:
+            count_0 += 1
             folder_name = row["folder"]
+            if count_0 not in list(range(start_row, end_row)):
+                continue
             if len(doc) > 0:
-                with open(os.path.join(output, f"{row['folder']}_mets.xml"), "w") as file:
+                target_path = Path(os.path.join(output, metadata_df.loc[index-1, 'folder'], 'images', f"master_EMT_{metadata_df.loc[index-1, 'folder']}_media"))
+                target_path.mkdir(parents=True, exist_ok=True)
+                with open(os.path.join(output, metadata_df.loc[index-1, 'folder'], "meta.xml"), "w") as file:
                     author, adressee = row["weranwen"].split(" an ")
                     print([d.values for d in doc])
                     file.write(template.render(docs=[d.values for d in doc], author=map_authors[author], adressee=map_authors[adressee], **doc[0]))
+                    src_files = os.listdir(os.path.join(folder, metadata_df.loc[index-1, 'folder']))
+                    for file_name in src_files:
+                        print(file_name)
+                        print(src_files)
+                        full_file_name = os.path.join(folder, metadata_df.loc[index-1, 'folder'], file_name)
+                        if os.path.isfile(full_file_name):
+                            shutil.copyfile(full_file_name, os.path.join(output, metadata_df.loc[index-1, 'folder'], "images", f"master_EMT_{metadata_df.loc[index-1, 'folder']}_media", file_name))
             doc = [row]
         else:
             doc.append(row)
