ed7156a2c2c5736a62d4588284065343b3cf6aa4
csae8092 <peter.andorfer@oeaw.ac.at>
2021-10-27 13:31:59 +0200

    fix for error in author lookup

--- create_mets_v2.py
+++ create_mets_v2.py
@@ -18,29 +18,43 @@ main_df = pd.read_csv('enriched_gesamt.csv')
 current_date = f"{date.today()}"
 for gr, df in main_df.dropna(subset=['folder']).groupby('folder'):
     row = df.iloc[0]
-    target_path = Path(
-        os.path.join(output, row['folder'], 'images', f"EMT_{row['folder']}_master")
-    )
-    target_path.mkdir(parents=True, exist_ok=True)
-    try:
-        author, adressee = row["weranwen"].split(" an ")
-    except AttributeError:
-        author, adressee = 'NN', 'NN'
-    docs = []
-    with open(os.path.join(output, row['folder'], "meta.xml"), "w") as file:
-        for i, cur_row in df.iterrows():
-            cur_file_name = cur_row['Dateiname']
-            docs.append(dict(cur_row))
-            source_file = [x for x in files if x.endswith(cur_file_name)][0]
-            target_file = os.path.join(target_path, cur_file_name)
-            print(f"copy {source_file} to {target_file}")
-            shutil.copyfile(source_file, target_file)
-        file.write(
-            template.render(
-                docs=[d for d in docs],
-                author=map_authors[author],
-                adressee=map_authors[adressee],
-                current_date=current_date,
-                **dict(row)
+    if "Kasten_blau_45_9_" in row['folder']:
+        target_path = Path(
+            os.path.join(
+                output, row['folder'], 'images', f"EMT_{row['folder']}_master"
             )
         )
+        target_path.mkdir(parents=True, exist_ok=True)
+        if " an " in row['weranwen']:
+            try:
+                author, adressee = row["weranwen"].split(" an ")
+            except AttributeError:
+                author, adressee = 'NN', 'NN'
+        else:
+            author, adressee = 'NN', 'NN'
+        docs = []
+        with open(os.path.join(output, row['folder'], "meta.xml"), "w") as file:
+            for i, cur_row in df.iterrows():
+                cur_file_name = cur_row['Dateiname']
+                docs.append(dict(cur_row))
+                source_file = [x for x in files if x.endswith(cur_file_name)][0]
+                target_file = os.path.join(target_path, cur_file_name)
+                print(f"copy {source_file} to {target_file}")
+                shutil.copyfile(source_file, target_file)
+                try:
+                    author_lk = map_authors[author]
+                except KeyError:
+                    author = "NN"
+                try:
+                    addr_lk = map_authors[adressee]
+                except KeyError:
+                    adressee = "NN"
+            file.write(
+                template.render(
+                    docs=[d for d in docs],
+                    author=map_authors[author],
+                    adressee=map_authors[adressee],
+                    current_date=current_date,
+                    **dict(row)
+                )
+            )
